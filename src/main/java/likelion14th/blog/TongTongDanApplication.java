30e491d0c46244bef2ad6caf63b83cf20d274f0c
김용현 <yongzik0515@naver.com>
2026-07-15 11:37:45 +0900

    Spring

---
+++ src/main/java/likelion14th/blog/TongTongDanApplication.java
@@ -0,0 +1,13 @@
+package likelion14th.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TongTongDanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TongTongDanApplication.class, args);
+    }
+
+}
