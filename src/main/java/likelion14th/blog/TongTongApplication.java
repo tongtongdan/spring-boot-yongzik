905a2c6b34cce8a8155920a82580774ca09567f4
김용현 <yongzik0515@naver.com>
2026-07-23 11:58:33 +0900

    initial commit

---
+++ src/main/java/likelion14th/blog/TongTongApplication.java
@@ -0,0 +1,13 @@
+package likelion14th.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TongTongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TongTongApplication.class, args);
+    }
+
+}
